7300b08846601fb328b79d5c11e8df349747c093
Matvey <matveyshalnev2010@gmail.com>
2026-06-15 00:08:33 +0300

    my commit

---
+++ weq/hw.py
@@ -0,0 +1,40 @@
+#
+#a = int(input("Введіть число: "))
+#print("Квадрат числа:", a * a)
+
+##
+# a = int(input(" перше число: "))
+# b = int(input(" друге число: "))
+# c = int(input(" третє число: "))
+# print((a+b+c)/3)
+
+###
+# min = int(input("Введіть кількість хвилин:"))
+# print(min // 60, "hours", min % 60,"minutes" )
+
+####
+
+# price = int(input("Введіть ціну:"))
+# discount = int(input("Введіть знижку (%): "))
+# print("Ціна зі знижкою:", price - price * discount // 100)
+
+#####
+# number = int(input("Введіть число:"))
+# print("Остання цифра:", number % 10 )
+
+######
+# a=int(input("Введіть довжину:"))
+# b=int(input("Введіть ширину:"))
+# print("Периметр:", 2 * (a + b))
+
+#######
+
+# w= int(input("Введіть 4-значне число: "))
+# a = w // 1000
+# b = (w // 100) % 10
+# c = (w //100) % 10
+# d = w % 10
+# print(a)
+# print(b)
+# print(c)
+# print(d)
---
+++ weq/lessson 2.py
@@ -0,0 +1,104 @@
+from operator import truediv
+from token import NUMBER
+
+# number: int = 10 + 1
+# print(number)
+# print(type(number))
+# number = 20.5
+# print(number)
+# print(type(number))
+# number = "Vasya"
+# print(number)
+# print(type(number))
+# number = True
+# print(number)
+# print(type(number))
+
+# let num = 10
+# num = 30;
+
+# my_site.com?param1=123&param2=qwerty
+
+NUMBER = 10
+print(NUMBER)
+
+# pep
+
+#
+# a = 10
+# b = 20
+# first_number = 10
+# second_number = 20
+# #
+# ###
+#userName1 = "Vasya"
+#print(userName1)
+#user_name = "Petya"
+#print(user_name)
+
+####
+#number1 = 10
+#print(number1)
+#print(number := 10 + number1)   # моржовій оператор
+#print(number)
+
+# num2 = 3
+# result = 2 + (num1 :=4) + num2
+# print(result)
+# print(num1)
+
+######
+# + - * /
+# print(2+3)
+# print(2-3)
+# print(2*3)
+# print(2/3)
+# print(2 ** 3)
+# print(2 // 3)
+# print(2 % 3)
+# ##
+# num1 = 19
+# num2 = 6
+# print(num1 // num2) #3
+# print(num1 % num2)  #1
+
+############
+
+# number = 456
+# n1 = number // 100 #4
+# n2 = number % 100 // 10 #5
+# n2_v2 = number % 10 // 100
+# n3 = number % 10 # 6
+# #
+# print(n1)
+# print(n2)
+# print(n2_v2)
+# print(n3)
+
+# number = 123
+# n1 = number // 100 #4
+# n2 = number % 100 // 10 #5
+# n2_v2 = number // 10 % 10
+# n3 = number % 10 # 6
+# #
+# print(n1)
+# print(n2)
+# print(n2_v2)
+# print(n3)
+# #
+
+# first_number = int(input("Enter first number: "))
+# second_number = int(input("Enter second number: "))
+# result = first_number + second_number
+# print(result)
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+# v1
+print("Hello,", name, "You are", age, "years old!")
+# v2
+print("Hello," + name, " You are " + str(age) + " years old!")
+# v3
+print(f"Hello,{name}. You are {age} years old!") 
+
+
