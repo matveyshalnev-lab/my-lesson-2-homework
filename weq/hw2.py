f78feead8be12731ee390219ceaf0f56e5a9eb4e
Matvey <matveyshalnev2010@gmail.com>
2026-06-18 14:12:12 +0300

    my commit2

---
+++ weq/hw2.py
@@ -0,0 +1,21 @@
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+
+operation = input("Введіть операцію (+, -, *, /): ")
+if operation == "+":
+    result = num1 + num2
+    print(result)
+    if operation == "-":
+        result = num1 - num2
+        print(result)
+        if operation == "*":
+            result = num1 * num2
+            print(result)
+            if operation == "/":
+                result = num1 / num2
+                print(result)
+            else:
+                print("Помилка!!! ділення на нуль неможливе")
+
+        else:
+            print("Помилка!!! невідома операція")
