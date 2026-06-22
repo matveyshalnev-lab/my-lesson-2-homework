c5aa63cf82fb70557fc1985a50d1d259de92aa75
Matvey <matveyshalnev2010@gmail.com>
2026-06-22 18:52:44 +0300

    my commit21`

---
+++ weq/hw4.py
@@ -0,0 +1,14 @@
+#4.1
+# list = [0,1,3,0,9]
+# zeros = list.count(0)
+# result = [x for x in list if x != 0] + [0] * zeros
+# print(result)
+
+#4.2
+# list = [0,1,7,2,4,8]
+# if len(list) == 0:
+#     result = 0
+# else:
+#     result = sum(list[::2]) * list[-1]
+#     print(result)
+
