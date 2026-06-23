fa2dadc70188bf10e3c9f95b35d25ddb5fa08e28
Matvey <matveyshalnev2010@gmail.com>
2026-06-23 23:51:17 +0300

    my commit21`

--- weq/hw 3.py
+++ weq/hw 3.py
@@ -1,1 +1,11 @@
-list = [1,2,3,4,5,]
+#3.2
+# def replace_last(lst):
+#     if len(lst) <= 1:
+#         return lst
+#     return [lst[-1]] + lst[:-1]
+#
+# #3.3
+
+def split_list(numbers):
+    middle = (len(numbers) + 1) // 2
+    return [numbers[:middle], numbers[middle:]]
